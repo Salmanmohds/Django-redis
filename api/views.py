905fad4b73b6574a873c4a1da894b5be23a744f8
Salmanmohds <salmangit83@gmail.com>
2021-12-05 15:08:45 +0530

    Code committed for Djangoredis

--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ import json
 from django.conf import settings
 import redis
 from rest_framework.decorators import api_view
-from rest_framework import status
 from rest_framework.response import Response
 
 # Connect to our Redis instance
